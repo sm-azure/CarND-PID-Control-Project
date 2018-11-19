73151f278174ed91895e1f66f1b744f495c1fda8
sm-azure <svanavi@DESKTOP-MJJ9189.localdomain>
2018-11-19 07:17:43 +0530

    initialize vars

--- src/PID.cpp
+++ src/PID.cpp
@@ -6,14 +6,30 @@ using namespace std;
 * TODO: Complete the PID class.
 */
 
-PID::PID() {}
+PID::PID() {
+    p_error = 0.0;
+    i_error = 0.0;
+    d_error = 0.0;
+
+    Kp = 0.0;
+    Ki = 0.0;
+    Kd = 0.0;
+}
 
 PID::~PID() {}
 
-void PID::Init(double Kp, double Ki, double Kd) {
+void PID::Init(double p, double i, double d) {
+    Kp = p;
+    Ki = i;
+    Kd = d;
+
+    p_error = 0.0;
+    i_error = 0.0;
+    d_error = 0.0;
 }
 
 void PID::UpdateError(double cte) {
+    
 }
 
 double PID::TotalError() {
